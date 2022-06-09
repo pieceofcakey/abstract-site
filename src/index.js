4d37ba2914e98d0e2852d3136f813b95fb2eb136
pieceofcakey <choi_927@naver.com>
2022-06-09 19:04:02 +0900

    3주차 과제 커밋

--- src/index.js
+++ src/index.js
@@ -1,10 +1,76 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        if (this.boards.find((existingBoard) => existingBoard.name === board.name)) {
+            throw Error();
+        }
+        board.siteAdded = true;
+        this.boards.push(board);
+    }
 
-class Article {}
+    findBoardByName(name) {
+        return this.boards.find((existingBoard) => existingBoard.name === name);
+    }
+}
 
-class Comment {}
+class Board {
+    constructor(name) {
+        if (!name) {
+            throw Error();
+        }
+        this.name = name;
+        this.siteAdded = false;
+        this.articles = [];
+    }
+    publish(article) {
+        if (this.siteAdded == false) {
+            throw Error();
+        }
+        article.id = `${this.name}-${Math.random()}`;
+        article.createdDate = new Date().toISOString();
+        this.articles.push(article);
+    }
+    getAllArticles() {
+        return this.articles;
+    }
+}
+
+class Article {
+    constructor(article) {
+        const { subject, content, author } = article;
+        if (!subject || !content || !author) {
+            throw Error();
+        }
+        this.subject = subject;
+        this.content = content;
+        this.author = author;
+        this.comments = [];
+    }
+    reply(comment) {
+        if (!this.id) {
+            throw Error();
+        }
+        comment.createdDate = new Date().toISOString();
+        this.comments.push(comment);
+    }
+    getAllComments() {
+        return this.comments;
+    }
+}
+
+class Comment {
+    constructor(comment) {
+        const { content, author } = comment;
+        if (!content || !author) {
+            throw Error();
+        }
+        this.content = content;
+        this.author = author;
+    }
+}
 
 module.exports = {
     Site,
